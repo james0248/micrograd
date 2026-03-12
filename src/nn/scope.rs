1179c60c66fdb9522ac74f079c0e1923d1e57fac
Hyeonseok <hsjames0248@gmail.com>
2026-03-13 01:33:54 +0900

    Update scope to use thread local scope

--- src/nn/scope.rs
+++ src/nn/scope.rs
@@ -1,8 +1,13 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
 
 use crate::{nn::tree::TensorTree, tensor::Tensor};
 
 use rand::rngs::StdRng;
+
+thread_local! {
+    static SCOPE: RefCell<Option<Scope>> = RefCell::new(None);
+}
 
 enum RunMode {
     Init,
@@ -15,22 +20,31 @@ pub struct Scope {
     prng_key: StdRng,
 }
 
-impl Scope {
-    pub fn params<F>(&mut self, name: &str, init_fn: F, shape: Vec<usize>) -> Tensor
-    where
-        F: Fn(&mut StdRng, Vec<usize>) -> Tensor,
-    {
-        match self.run_mode {
+pub fn param<F>(name: &str, init_fn: F, shape: Vec<usize>) -> Tensor
+where
+    F: Fn(&mut StdRng, Vec<usize>) -> Tensor,
+{
+    SCOPE.with(|cell| {
+        let mut maybe_scope = cell.borrow_mut();
+        let scope = maybe_scope
+            .as_mut()
+            .expect("Called param outside of init() or apply()");
+
+        match scope.run_mode {
             RunMode::Init => {
-                let param = init_fn(&mut self.prng_key, shape);
-                self.params
+                let param = init_fn(&mut scope.prng_key, shape);
+                scope
+                    .params
                     .insert(Box::from(name), TensorTree::Leaf(param.clone()));
 
                 param
             }
 
             RunMode::Apply => {
-                let param = self.params.get(name).expect("Parameter not found");
+                let param = scope
+                    .params
+                    .get(name)
+                    .unwrap_or_else(|| panic!("Parameter {} not found in scope", name));
 
                 match param {
                     TensorTree::Leaf(tensor) => tensor.clone(),
@@ -38,5 +52,5 @@ impl Scope {
                 }
             }
         }
-    }
+    })
 }
